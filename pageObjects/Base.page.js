6b4c57cc59792cae9bdf00ed0d56d9307c249db1
AlisaGrinko <63740096+AlisaGrinko@users.noreply.github.com>
2020-07-22 21:11:34 -0400

    TLC-234 (#13)

--- pageObjects/Base.page.js
+++ pageObjects/Base.page.js
@@ -1,13 +1,5 @@
-/**
-* main page object containing all methods, selectors and functionality
-* that is shared across all page objects
-*/
-module.exports = class Page {
-    /**
-    * Opens a sub page of the page
-    * @param path path of the sub page (e.g. /path/to/page.html)
-    */
-    open (path) {
-        return browser.url(`https://stage.localcoding.us/${path}`)
+export default class BasePage {
+    open(path) {
+        browser.url(path);
     }
 }

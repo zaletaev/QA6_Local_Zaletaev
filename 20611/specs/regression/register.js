9e2305ea7340172c1c00f5c01878f19a6d8f6e5c
Kseniia855 <knsydorenko@gmail.com>
2020-07-24 19:40:21 -0400

    Second commit

--- 20611/specs/regression/register.js
+++ 20611/specs/regression/register.js
@@ -5,10 +5,21 @@ import RegisterPage from '../../../pageObjects/register.page';
 describe('New user registration page', () => {
   it('Check if the title of the page is Local Coding', () => {
     RegisterPage.open();
-    expect(browser.getTitle()).eq('Local Coding');
+    expect(browser.getTitle()).to.equal('Local Coding');
   });
-  it('Check if the title of the page is Local Coding', () => {
-    RegisterPage.open();
-    expect(browser.getTitle()).eq('Local Coding');
+  it('Check if the main text on the page is Create an account', () => {
+    expect(RegisterPage.headerRegister.getText()).to.equal('Create an account');
+  });
+  it('Check if the First Name textbox has a placeholder text First Name ', function() {
+    expect(RegisterPage.firstNameInput.getAttribute('placeholder')).to.equal('First Name');
+  });
+  it('Check if the Last Name textbox has a placeholder text Last Name ', function() {
+    expect(RegisterPage.lastNameInput.getAttribute('placeholder')).to.equal('Last Name');
+  });
+  it('Check if the Email textbox has a placeholder text Email ', function() {
+    expect(RegisterPage.email.getAttribute('placeholder')).to.equal('Email');
+  });
+  it('Check if the Password textbox has a placeholder text Password ', function() {
+    expect(RegisterPage.password.getAttribute('placeholder')).to.equal('Password');
   });
 });

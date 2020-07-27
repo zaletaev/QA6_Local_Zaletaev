f2fe730ff27f6a28cd83b1cac69421857f10debd
MaslovStan <57243009+MaslovStan@users.noreply.github.com>
2020-07-26 23:30:28 -0700

    TLC-275 refactor register.js spec (#51)

--- 20609/specs/smoke/register.js
+++ 20609/specs/smoke/register.js
@@ -2,7 +2,7 @@ import {headers, userNew} from '../data/registerData';
 import RegisterPage from '../../../pageObjects/register.page'
 import RegisterPageStep2 from '../../../pageObjects/registerStep2.page';
 import LoginPage from '../../../pageObjects/login.page'
-
+import ProfilePage from '../../../pageObjects/profile.page'
 describe('register new user', () => {
   before( () => {
    RegisterPage.open()
@@ -12,20 +12,20 @@ describe('register new user', () => {
     expect(RegisterPage.headerRegister.getText()).eq(headers.headerRegister)
   });
 
-  it('step 1 should fill out filds and click submit btn', () => {
+  it('step 1 should fill out fields and click submit btn', () => {
    RegisterPage.registerUser(userNew);
    browser.waitUntil(()=> RegisterPageStep2.headerRegisterStep2.getText()===headers.headerStep2Page)
   });
 
-  it('step 2 should fill out phone fild and click submit btn ', () => {
+  it('step 2 should fill out phone field and click submit btn ', () => {
     RegisterPageStep2.registerUserStep2(userNew)
-    browser.waitUntil(()=> $('h1').getText()===userNew.headerName)
+    browser.waitUntil(()=> ProfilePage.headerProfile.getText()===userNew.headerName)
   });
 
   it('should login new user', () => {
     LoginPage.open()
     LoginPage.login(userNew.email,userNew.password)
-    browser.waitUntil(()=> $('h1').getText()===userNew.headerName)
+    browser.waitUntil(()=> ProfilePage.headerProfile.getText()===userNew.headerName)
   });
 
 });
